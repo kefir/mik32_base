1e87d2445b2399c10e704235ffeb07f6bcc8e9e5
Sergey Tkachenko <sa.tkachenko@mettem-m.ru>
2022-11-07 16:33:32 +0300

    Library update fix

--- include/target.h
+++ include/target.h
@@ -8,11 +8,11 @@
 #include "csr.h"
 #include "scr1_csr_encoding.h"
 
-#include "epic.h"
-#include "gpio.h"
-#include "lptim.h"
-#include "pad_config.h"
-#include "power_manager.h"
+#include "mik32_epic.h"
+#include "mik32_gpio.h"
+#include "mik32_lptim.h"
+#include "mik32_pad_config.h"
+#include "mik32_power_manager.h"
 
 #define LED1_PIN 0
 #define LED2_PIN 0
--- target/target.c
+++ target/target.c
@@ -2,19 +2,22 @@
 
 #include "timers.h"
 
+#include "mik32_spi.h"
+
 static void gpio_init(void);
 static void system_init(void);
+static void spi0_init(void);
 
 static void gpio_init(void)
 {
-    PM->CLK_APB_P_SET |= PM_CLOCK_GPIO_1_M | PM_CLOCK_GPIO_0_M; // включение тактирования GPIO0 и GPIO1
-    PM->CLK_APB_M_SET |= PM_CLOCK_PAD_CONFIG_M; // включение тактирования блока для смены режима выводов
+    PM->CLK_APB_P_SET |= PM_CLOCK_GPIO_1_M | PM_CLOCK_GPIO_0_M;
+    PM->CLK_APB_M_SET |= PM_CLOCK_PAD_CONFIG_M;
 
-    PAD_CONFIG->PORT_1_CFG |= (1 << (2 * LED1_PIN)); // Установка порта 1 в режим GPIO
-    PAD_CONFIG->PORT_0_CFG |= (1 << (2 * LED2_PIN)); // Установка порта 1 в режим GPIO
+    PAD_CONFIG->PORT_1_CFG |= (1 << (2 * LED1_PIN));
+    PAD_CONFIG->PORT_0_CFG |= (1 << (2 * LED2_PIN));
 
-    GPIO_1->DIRECTION_OUT = 0xFFFF; // Установка направления порта 1 в выход
-    GPIO_0->DIRECTION_OUT = 0xFFFF; // Установка направления порта 1 в выход
+    GPIO_1->DIRECTION_OUT = 0xFFFF;
+    GPIO_0->DIRECTION_OUT = 0xFFFF;
 
     GPIO_1->OUTPUT |= (1 << LED1_PIN);
     GPIO_0->OUTPUT |= (1 << LED2_PIN);
@@ -26,9 +29,36 @@ static void system_init(void)
 
     gpio_init();
     timers_init();
+    spi0_init();
 
     write_csr(mstatus, MSTATUS_MIE);
     write_csr(mie, MIE_MEIE);
+}
+
+static void spi0_init(void)
+{
+    PM->CLK_APB_P_SET |= PM_CLOCK_SPI_0_M;
+
+    SPI_0->Enable = ~SPI_ENABLE_M;
+
+    SPI_0->Config = SPI_CONFIG_BAUD_RATE_DIV_32_M
+        | SPI_CONFIG_CS_0_M
+        | SPI_CONFIG_MASTER_M;
+
+    SPI_0->TxThr = 1;
+
+    SPI_0->Enable |= SPI_ENABLE_M;
+
+    SPI_0->TxData = 0x03;
+    while ((SPI_0->IntStatus & SPI_TX_FIFO_not_full_M)) {
+    }
+
+    SPI_0->TxData = 0x0C;
+    while ((SPI_0->IntStatus & SPI_TX_FIFO_not_full_M)) {
+    }
+
+    volatile uint8_t lolol = SPI_0->RxData;
+    (void)lolol;
 }
 
 void target_init(void)
